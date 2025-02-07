34987e6dca7391cb6b249506b306608af39ff715
Nataliia Kolosova <natvol4ica@gmail.com>
2025-02-07 01:11:03 +0100

    words without collection are added to default collection on app launch

--- lib/local_db/seed/db_seed.dart
+++ lib/local_db/seed/db_seed.dart
@@ -1,28 +1,50 @@
 import 'package:dutch_app/local_db/db_context.dart';
+import 'package:dutch_app/local_db/entities/db_word.dart';
 import 'package:dutch_app/local_db/entities/db_word_collection.dart';
 import 'package:isar/isar.dart';
 
-class CollectionConfig {
+class CollectionsConfig {
   static int defaultCollectionId = -1;
+  static const String defaultCollectionName = "Words without collection";
 }
 
-Future<int> seedDatabaseAsync() async {
+Future<void> seedDatabaseAsync() async {
+  DbWordCollection defaultCollection =
+      await getOrCreateDefaultCollectionIdAsync();
+  CollectionsConfig.defaultCollectionId = defaultCollection.id!;
+
+  await setDefaultCollectionForExistingWordsAsync(defaultCollection);
+}
+
+Future<DbWordCollection> getOrCreateDefaultCollectionIdAsync() async {
   var isar = DbContext.isar;
-  final DbWordCollection? defaultCollection =
-      await isar.dbWordCollections.filter().nameEqualTo("Default").findFirst();
+  final DbWordCollection? defaultCollection = await isar.dbWordCollections
+      .filter()
+      .nameEqualTo(CollectionsConfig.defaultCollectionName)
+      .findFirst();
 
   if (defaultCollection != null) {
-    CollectionConfig.defaultCollectionId = defaultCollection.id!;
-    return defaultCollection.id!;
+    return defaultCollection;
   }
 
   var newCollection = DbWordCollection();
-  newCollection.name = "Default";
+  newCollection.name = CollectionsConfig.defaultCollectionName;
 
   await isar.writeTxn(() async {
     await isar.dbWordCollections.put(newCollection);
   });
 
-  CollectionConfig.defaultCollectionId = newCollection.id!;
-  return newCollection.id!;
+  return newCollection;
+}
+
+Future<void> setDefaultCollectionForExistingWordsAsync(
+    DbWordCollection defaultCollection) async {
+  var isar = DbContext.isar;
+  List<DbWord> wordsWithoutCollections =
+      await isar.dbWords.filter().collectionIsNull().findAll();
+
+  await DbContext.isar.writeTxn(() async {
+    defaultCollection.words.addAll(wordsWithoutCollections);
+    defaultCollection.words.save();
+  });
 }
