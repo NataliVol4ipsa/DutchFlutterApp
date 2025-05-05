71584aac9ac5c8e19a649c5e59119c550e4b54fe
Nataliia Kolosova <natvol4ica@gmail.com>
2025-05-05 17:06:21 +0200

    cleanup data migration files

--- lib/core/local_db/seed/db_seed.dart
+++ lib/core/local_db/seed/db_seed.dart
@@ -1,11 +1,7 @@
 import 'package:dutch_app/core/local_db/seed/db_default_collection_seed.dart';
 import 'package:dutch_app/core/local_db/seed/db_dutch_words_seed.dart';
-import 'package:dutch_app/core/local_db/seed/db_word_migration_phase1.dart';
-import 'package:dutch_app/core/local_db/seed/db_word_migration_phase2.dart';
 
 Future<void> seedDatabaseAsync() async {
   await DbDefaultCollectionSeed.seedAsync();
   await DbDutchWordsSeed.seedAsync();
-  //await DbWordMigrationPhase1.runAsync();
-  //await DbWordMigrationPhase2.runAsync();
 }
